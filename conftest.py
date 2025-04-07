83497af4c578b2451a92229b381b34c103ba12cc
term51 <webworktoday51@gmail.com>
2025-04-07 16:23:55 +0300

    added allure steps to base_page added requirements updated gitignore, conftest

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
+import datetime
 import pytest
+import allure
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,3 +13,41 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+# хук, который срабатывает после каждого теста
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    # получаем отчёт
+    outcome = yield
+    rep = outcome.get_result()
+
+    # интересует только фаза исполнения теста (call), а не setup/teardown
+    if rep.when == 'call' and rep.failed:
+        # пытаемся достать фикстуру driver
+        driver = item.funcargs.get('driver')
+        if driver:
+            png = driver.get_screenshot_as_png()
+            now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            allure.attach(
+                png,
+                name=f'screenshot_{item.name}_{now}',
+                attachment_type=allure.attachment_type.PNG
+            )
+
+## Автоматическая генерация метаданных для Allure
+# @pytest.fixture(autouse=True)
+# def allure_metadata(request):
+#     file_path = request.fspath  # Получаем полный путь к файлу теста
+#     file_name = os.path.basename(file_path).replace('.py', '')
+#     suite_name = file_name.replace('_test', '').replace('_', ' ')
+#
+#     test_name = request.node.name  # Имя теста, например, "test_text_box"
+#     feature = test_name.replace('test_', '').replace('_', ' ').title()
+#     title = f"Check {feature}"
+#     print('suite', suite_name, 'feature', feature, 'title', title)
+#
+#     with allure.step(f"Running {title}"):
+#         allure.dynamic.suite(suite_name)
+#         allure.dynamic.feature(feature)
+#         allure.dynamic.title(title)
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,23 +12,24 @@ class BasePage:
         self.poll_frequency = 1
 
     def open(self):
-        self.driver.get(self.url)
+        with allure.step(f"Open the page: {self.url}"):
+            self.driver.get(self.url)
 
+    @allure.step("Wait for element to be visible: {locator}")
     def element_is_visible(self, locator, timeout=None):
-        """
-             visibility_of_element_located - Ожидание проверки того, что элемент присутствует в DOM страницы и виден.
-             Видимость означает, что элемент не только отображается, но и имеет высоту и ширину больше 0.
-         """
         self.go_to_element(self.element_is_present(locator))
+        # with allure.step(f"Wait for element to be visible: {locator}"):
         return wait(self.driver, timeout or self.default_timeout, poll_frequency=self.poll_frequency).until(
             EC.visibility_of_element_located(locator)
         )
 
+    @allure.step("Wait for elements to be visible: {locator}")
     def elements_are_visible(self, locator, timeout=None):
         return wait(self.driver, timeout or self.default_timeout, poll_frequency=self.poll_frequency).until(
             EC.visibility_of_all_elements_located(locator)
         )
 
+    @allure.step("Wait for element to be present in DOM: {locator}")
     def element_is_present(self, locator, timeout=None):
         """
             presence_of_element_located - Ожидание проверки того, что элемент присутствует в DOM страницы.
@@ -37,53 +39,59 @@ class BasePage:
             EC.presence_of_element_located(locator)
         )
 
+    @allure.step('Wait for elements to be present in DOM: {locator}')
     def elements_are_present(self, locator, timeout=None):
         return wait(self.driver, timeout or self.default_timeout, poll_frequency=self.poll_frequency).until(
             EC.presence_of_all_elements_located(locator)
         )
 
+    @allure.step('Wait for element to become invisible: {locator}')
     def element_is_not_visible(self, locator, timeout=None):
         return wait(self.driver, timeout or self.default_timeout, poll_frequency=self.poll_frequency).until(
             EC.invisibility_of_element(locator)
         )
 
+    @allure.step('Wait for element to be clickable: {locator}')
     def element_is_clickable(self, locator, timeout=None):
         return wait(self.driver, timeout or self.default_timeout, poll_frequency=self.poll_frequency).until(
             EC.element_to_be_clickable(locator)
         )
 
     def go_to_element(self, element):
-        """ JS scroll to element """
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+        with allure.step(f"Scroll to element: {element.tag_name}.{element.get_attribute("class")}"):
+            """ JS scroll to element """
+            self.driver.execute_script("arguments[0].scrollIntoView();", element)
 
     def action_double_click(self, element):
-        action = ActionChains(self.driver)
-        action.double_click(element)
-        action.perform()
+        with allure.step(f"Double click on element: {element.tag_name}.{element.get_attribute("class")}"):
+            ActionChains(self.driver).double_click(element).perform()
 
     def action_right_click(self, element):
-        action = ActionChains(self.driver)
-        action.context_click(element)
-        action.perform()
+        with allure.step(f"Right click on element: {element.tag_name}.{element.get_attribute("class")}"):
+            ActionChains(self.driver).context_click(element).perform()
 
+    @allure.step('Switch to new browser tab')
     def switch_to_new_tab(self):
         tabs = self.driver.window_handles
         self.driver.switch_to.window(tabs[1])
 
+    @allure.step('Get browser alert')
     def get_alert(self):
         return self.driver.switch_to.alert
 
     def action_drag_and_drop_by_offset(self, element, x, y):
-        action = ActionChains(self.driver)
-        action.drag_and_drop_by_offset(element, x, y)
-        action.perform()
+        with allure.step(
+                f"Drag and drop element: {element.tag_name}.{element.get_attribute("class")} by offset (x={x}, y={y})'"
+        ):
+            ActionChains(self.driver).drag_and_drop_by_offset(element, x, y).perform()
 
     def action_drag_and_drop_to_element(self, source, target):
-        action = ActionChains(self.driver)
-        action.drag_and_drop(source, target)
-        action.perform()
+        with allure.step(
+                f"Drag and drop element: {source.tag_name}.{source.get_attribute("class")} "
+                f"to target: {target.tag_name}.{target.get_attribute('class')}"
+        ):
+            ActionChains(self.driver).drag_and_drop(source, target).perform()
 
     def action_move_to_element(self, element):
-        action = ActionChains(self.driver)
-        action.move_to_element(element)
-        action.perform()
+        with allure.step(f"Move mouse to element: {element.tag_name}.{element.get_attribute("class")}"):
+            ActionChains(self.driver).move_to_element(element).perform()
